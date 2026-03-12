f87bfaf3b173ca0e66fa1f7daaa118a8fa6386ae
Topi-M <topi.martikainen04@gmail.com>
2026-03-12 14:15:38 +0200

    Cubiomes Wasm käännös

---
+++ wasm/cubiomes_wrapper.c
@@ -0,0 +1,32 @@
+#include "cubiomes/generator.h"
+#include "cubiomes/util.h"
+#include <emscripten.h>
+#include <stdlib.h>
+
+static Generator g;
+
+EMSCRIPTEN_KEEPALIVE
+void initGenerator(int mcVersion, int64_t seed) {
+    setupGenerator(&g, mcVersion, 0);
+    applySeed(&g, DIM_OVERWORLD, (uint64_t)seed);
+}
+
+EMSCRIPTEN_KEEPALIVE
+int* getBiomeMap(int x, int z, int width, int height, int scale) {
+    int* biomes = (int*)malloc(width * height * sizeof(int));
+    Range r;
+    r.scale = scale;
+    r.x = x;
+    r.z = z;
+    r.sx = width;
+    r.sz = height;
+    r.y = 63;
+    r.sy = 1;
+    genBiomes(&g, biomes, r);
+    return biomes;
+}
+
+EMSCRIPTEN_KEEPALIVE
+void freePtr(void* ptr) {
+    free(ptr);
+}
