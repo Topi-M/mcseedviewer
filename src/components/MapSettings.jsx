ca9deb574c1dd6bdc31954fa553158de8480356a
Topi-M <topi.martikainen04@gmail.com>
2026-03-20 17:15:05 +0200

    Map Settings tekstit käännetty

--- src/components/MapSettings.jsx
+++ src/components/MapSettings.jsx
@@ -3,7 +3,7 @@ import '../css/MapSettings.css'
 export default function MapSettings({ showRegions, onToggleRegions }) {
   return (
     <div className="map-settings">
-      <div className="map-settings-title">Kartta-asetukset</div>
+      <div className="map-settings-title">Map Settings</div>
       <div className="map-settings-toggles">
         <label className="map-settings-toggle">
           <input
@@ -11,7 +11,7 @@ export default function MapSettings({ showRegions, onToggleRegions }) {
             checked={showRegions}
             onChange={onToggleRegions}
           />
-          <span>Region-reunat (512x512)</span>
+          <span>Show Regions (512x512)</span>
         </label>
       </div>
     </div>
